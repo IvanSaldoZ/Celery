8a7de8c26a81e0ae1e3290e75b06c24d23350a6b
WinPCi9 <saldoz@ya.ru>
2021-08-07 00:22:33 +0300

    Обновил периодическую таску (тестирование)

--- send_email/main/tasks.py
+++ send_email/main/tasks.py
@@ -1,8 +1,11 @@
+import pathlib
+
 from django.core.mail import send_mail
-from send_email.celery import app
+from send_email.celery_mod import app
 
 from .services import send
 from .models import Contact
+
 
 @app.task
 def send_spam_email(user_email):
@@ -16,5 +19,11 @@ def send_spam_email(user_email):
 
 @app.task
 def send_beat_email():
+    """
+    Периодическая таска
+    :return:
+    """
     for contact in Contact.objects.all():
+        with open('email_sending.log', 'wt') as f:
+            f.write(contact.email)
         send(contact.email)
