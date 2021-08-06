efd8d30c540433e0fde64bb6aa271cb3df39ada2
WinPCi9 <saldoz@ya.ru>
2021-08-06 21:35:09 +0300

    Добавлен файл tasks.py, который содержит список тасков на выполнение Celery

---
+++ send_email/main/tasks.py
@@ -0,0 +1,15 @@
+from django.core.mail import send_mail
+from send_email.celery import app
+
+from .services import send
+from .models import Contact
+
+@app.task
+def send_spam_email(user_email):
+    """
+    Таска по отрпавке email-а
+    :param user_email:
+    :return:
+    """
+    send(user_email)
+
