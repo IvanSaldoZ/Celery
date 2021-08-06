24e88031ab84a9cfaba57f082b27a231d04afa5a
WinPCi9 <saldoz@ya.ru>
2021-08-06 21:04:59 +0300

    Добавлена функция в services.py, которая отправляет сообщение на указанный при подписке email

--- send_email/main/services.py
+++ send_email/main/services.py
@@ -1,5 +1,13 @@
-def send():
+from django.core.mail import send_mail
+
+def send(user_email):
     """
     Функция для отправки email-а
     :return:
     """
+    send_mail('Вы подписались на рассылку!',
+              'Мы будем присылать вам много спама!',
+              "saldoz@ya.ru",
+              [user_email],
+              fail_silently=False,
+              )
