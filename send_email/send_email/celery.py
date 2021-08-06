80d1ca7230880aaf042214d129f3dd814d43a637
WinPCi9 <saldoz@ya.ru>
2021-08-06 21:24:41 +0300

    Файл celery.py перемещен из папки приложения main в папку всего проекта send_email

--- send_email/send_email/celery.py
+++ send_email/send_email/celery.py
@@ -8,7 +8,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'send_email.settings')
 # Создаем объект селери, первый аргумент - это имя текущего модуля
 # https://docs.celeryproject.org/en/stable/getting-started/first-steps-with-celery.html#first-steps
 app = Celery('send_email')
-# Загружаем настройки из файла Django так, чтобы CELERY читал только те переменные, которые начинаются с CELERY
+# Загружаем настройки из файла Django так, чтобы CELERY читал только те переменные,
+# которые начинаются с CELERY (указывается в namespace)
 app.config_from_object('django.conf:settings', namespace='CELERY')
-# Автоматически подцепляем наши таски
+# Автоматически подцепляем наши таски из файла tasks.py
 app.autodiscover_tasks()
