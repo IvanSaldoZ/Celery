b6bd678ccff9c4f062692f35d69d46bc3d3847cb
WinPCi9 <saldoz@ya.ru>
2021-08-07 00:21:36 +0300

    Включил обычный режим подгрузки Celery

--- send_email/send_email/__init__.py
+++ send_email/send_email/__init__.py
@@ -1,3 +1,8 @@
-from .celery import app as celery_app
+from .celery_mod import app as celery_app
 
 __all__ = ('celery_app')
+
+# from __future__ import absolute_import, unicode_literals
+# from .celery_mod import app as celery_app
+#
+# __all__ = ('celery_app')
